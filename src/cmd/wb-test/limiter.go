0d326277d848bbf34e738afaf57038cd9b3cce04
twentydraft <clamores.pro@gmail.com>
2017-09-26 17:11:52 +0300

    limiter docs

--- src/cmd/wb-test/limiter.go
+++ src/cmd/wb-test/limiter.go
@@ -4,11 +4,18 @@ import (
 	"sync"
 )
 
+// limiter
+// Limits the number of goroutines
+// and allows you to wait
+// for the worker group to complete.
 type limiter struct {
 	waitGroup *sync.WaitGroup
 	limit     chan struct{}
 }
 
+// Returns new limiter which restrains
+// the size of worker group with number
+// less or equal k.
 func newLimiter(k uint) *limiter {
 	return &limiter{
 		waitGroup: &sync.WaitGroup{},
@@ -16,6 +23,10 @@ func newLimiter(k uint) *limiter {
 	}
 }
 
+// Adds a new worker to groups
+// and returns "done" function,
+// which must be run to decrement
+// worker counter.
 func (lim *limiter) Start() func() {
 	lim.waitGroup.Add(1)
 	lim.limit <- struct{}{}
@@ -25,11 +36,13 @@ func (lim *limiter) Start() func() {
 	}
 }
 
+// Blocks until all workers complete their tasks.
 func (lim *limiter) Wait() {
 	lim.waitGroup.Wait()
 	close(lim.limit)
 }
 
+// Returns a number of active workers.
 func (lim *limiter) Active() int {
 	return len(lim.limit)
 }
